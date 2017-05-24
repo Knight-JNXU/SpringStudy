c182d2d94cfceba143e6d6b91551f249da9fc113
wen.liu <wen.liu@baozun.cn>
2017-05-24 15:24:31 +0800

    去掉一些多余的包

--- src/main/java/code/controller/MenuController.java
+++ src/main/java/code/controller/MenuController.java
@@ -1,16 +1,8 @@
 package code.controller;
 
-import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 
 /**
